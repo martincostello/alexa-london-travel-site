53b0d6abd7bf57812cf83acbff01c3f1f0da5813
martincostello <martin@martincostello.com>
2017-03-05 19:37:56 +0000

    Add Travis script Add script to build in Travis CI.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
+export configuration=Release
+
+dotnet restore LondonTravel.Site.sln --verbosity minimal || exit 1
+dotnet build LondonTravel.Site.sln --output $artifacts --configuration $configuration || exit 1
+dotnet test tests/LondonTravel.Site.Tests/LondonTravel.Site.Tests.csproj --output $artifacts --configuration $configuration || exit 1
+dotnet publish src/LondonTravel.Site/LondonTravel.Site.csproj --output $artifacts --configuration $configuration || exit 1
