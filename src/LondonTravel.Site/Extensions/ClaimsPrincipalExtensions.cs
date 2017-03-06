adb9d2dc0dd73d6ef37442495f707ad854aeaec3
martincostello <martin@martincostello.com>
2017-03-06 10:21:06 +0000

    Add extension method for avatar URLs
    Add an extension method to generate avatar URLs for logged-in users
    using Gravatar.

---
+++ src/LondonTravel.Site/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.LondonTravel.Site.Extensions
+{
+    using System;
+    using System.Globalization;
+    using System.Security.Claims;
+    using System.Text;
+
+    /// <summary>
+    /// A class containing extension methods for the <see cref="ClaimsPrincipal"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class ClaimsPrincipalExtensions
+    {
+        /// <summary>
+        /// Gets the URL of the avatar image to use for the specified claims principal.
+        /// </summary>
+        /// <param name="value">The user to get the avatar image URL for.</param>
+        /// <param name="fallbackImageUrl">The URL of the default avatar image to use.</param>
+        /// <param name="size">The preferred size of the avatar image.</param>
+        /// <returns>
+        /// The URL to use to render the avatar for the specified user.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="value"/> or <paramref name="fallbackImageUrl"/> is <see langword="null"/>.
+        /// </exception>
+        public static string GetAvatarUrl(this ClaimsPrincipal value, string fallbackImageUrl, int size = 24)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (fallbackImageUrl == null)
+            {
+                throw new ArgumentNullException(nameof(fallbackImageUrl));
+            }
+
+            string email = value.FindFirst(ClaimTypes.Email)?.Value;
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return fallbackImageUrl;
+            }
+
+            string normalized = email.Trim().ToLowerInvariant();
+            byte[] buffer = Encoding.UTF8.GetBytes(normalized);
+
+            byte[] hash;
+
+            using (var algorithm = System.Security.Cryptography.MD5.Create())
+            {
+                hash = algorithm.ComputeHash(buffer);
+            }
+
+            string hashString = HashToString(hash);
+            string escapedFallback = Uri.EscapeUriString(fallbackImageUrl);
+
+            return FormattableString.Invariant($"https://www.gravatar.com/avatar/{hashString}?s={size}&d={escapedFallback}");
+        }
+
+        /// <summary>
+        /// Returns the string representation of the specified hash bytes.
+        /// </summary>
+        /// <param name="hash">An array containing the hash to convert to a string.</param>
+        /// <returns>
+        /// The string representation of <paramref name="hash"/>.
+        /// </returns>
+        private static string HashToString(byte[] hash)
+        {
+            var builder = new StringBuilder(hash.Length * 2);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                builder.AppendFormat(CultureInfo.InvariantCulture, "{0:x2}", hash[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ tests/LondonTravel.Site.Tests/Extensions/ClaimsPrincipalExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.LondonTravel.Site.Extensions
+{
+    using System;
+    using System.Security.Claims;
+    using Shouldly;
+    using Xunit;
+
+    /// <summary>
+    /// A class containing unit tests for the <see cref="ClaimsPrincipalExtensions"/> class.
+    /// </summary>
+    public static class ClaimsPrincipalExtensionsTests
+    {
+        private const string FallbackImageUrl = "https://www.mypicture.com/Hello World";
+
+        [Fact]
+        public static void GetAvatarUrl_Throws_If_Required_Parameters_Are_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ClaimsPrincipalExtensions.GetAvatarUrl(null, string.Empty)).ParamName.ShouldBe("value");
+            Assert.Throws<ArgumentNullException>(() => ClaimsPrincipalExtensions.GetAvatarUrl(new ClaimsPrincipal(), null)).ParamName.ShouldBe("fallbackImageUrl");
+        }
+
+        [Fact]
+        public static void GetAvatarUrl_Returns_Correct_Url_If_No_Email_Claim()
+        {
+            // Arrange
+            var principal = new ClaimsPrincipal();
+
+            // Act
+            string actual = principal.GetAvatarUrl(FallbackImageUrl);
+
+            // Assert
+            actual.ShouldBe(FallbackImageUrl);
+        }
+
+        [Theory]
+        [InlineData("", FallbackImageUrl)]
+        [InlineData("MyEmailAddress@example.com ", "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?s=24&d=https://www.mypicture.com/Hello%20World")]
+        public static void GetAvatarUrl_Returns_Correct_Url(string email, string expected)
+        {
+            // Arrange
+            var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Email, email) });
+            var principal = new ClaimsPrincipal(identity);
+
+            // Act
+            string actual = principal.GetAvatarUrl(FallbackImageUrl);
+
+            // Assert
+            actual.ShouldBe(expected);
+        }
+
+        [Fact]
+        public static void GetAvatarUrl_Returns_Correct_Url_With_Explicit_Size()
+        {
+            // Arrange
+            var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Email, "MyEmailAddress@example.com") });
+            var principal = new ClaimsPrincipal(identity);
+
+            // Act
+            string actual = principal.GetAvatarUrl(FallbackImageUrl, 42);
+
+            // Assert
+            actual.ShouldBe("https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?s=42&d=https://www.mypicture.com/Hello%20World");
+        }
+    }
+}
