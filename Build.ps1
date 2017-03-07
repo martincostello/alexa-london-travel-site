7724f44ba5d7685c1c78128f07372edc2450dfee
martin-costello <martin.costello@just-eat.com>
2017-03-07 10:59:52 +0000

    Update to .NET Core SDK 1.0.0
    Update to v1.0.0 of the .NET Core SDK.

--- Build.ps1
+++ Build.ps1
@@ -13,7 +13,7 @@ $ErrorActionPreference = "Stop"
 $solutionPath  = Split-Path $MyInvocation.MyCommand.Definition
 $solutionFile  = Join-Path $solutionPath "LondonTravel.Site.sln"
 $framework     = "netcoreapp1.1"
-$dotnetVersion = "1.0.0-rc4-004788"
+$dotnetVersion = "1.0.0"
 
 if ($OutputPath -eq "") {
     $OutputPath = "$(Convert-Path "$PSScriptRoot")\artifacts"
