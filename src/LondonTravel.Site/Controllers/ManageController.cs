5611ac468a7a03cf93351f55e786b9849cd28f4a
martincostello <martin@martincostello.com>
2017-03-08 23:08:17 +0000

    Initial account linking implementation
    Initial implementation of account linking.
    It seems to be being tempremental at the moment and not redirecting
    properly when clicking the linking buttons as sometimes it shows the
    sign-in page instead of just linking it straight away.

--- src/LondonTravel.Site/Controllers/ManageController.cs
+++ src/LondonTravel.Site/Controllers/ManageController.cs
@@ -3,8 +3,15 @@
 
 namespace MartinCostello.LondonTravel.Site.Controllers
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+    using MartinCostello.LondonTravel.Site.Identity;
+    using MartinCostello.LondonTravel.Site.Models;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
 
     /// <summary>
     /// A class representing the controller for the <c>/manage/</c> resource.
@@ -13,6 +20,21 @@ namespace MartinCostello.LondonTravel.Site.Controllers
     [Route("manage", Name = "Manage")]
     public class ManageController : Controller
     {
+        private readonly UserManager<LondonTravelUser> _userManager;
+        private readonly SignInManager<LondonTravelUser> _signInManager;
+        private readonly string _externalCookieScheme;
+
+        public ManageController(
+          UserManager<LondonTravelUser> userManager,
+          SignInManager<LondonTravelUser> signInManager,
+          IOptions<IdentityCookieOptions> identityCookieOptions,
+          ILoggerFactory loggerFactory)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _externalCookieScheme = identityCookieOptions.Value.ExternalCookieAuthenticationScheme;
+        }
+
         /// <summary>
         /// Gets the result for the <c>/manage/</c> action.
         /// </summary>
@@ -20,6 +42,100 @@ namespace MartinCostello.LondonTravel.Site.Controllers
         /// The result for the <c>/manage/</c> action.
         /// </returns>
         [HttpGet]
-        public IActionResult Index() => View();
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var userLogins = await _userManager.GetLoginsAsync(user);
+
+            var otherLogins = _signInManager.GetExternalAuthenticationSchemes()
+                .Where((p) => userLogins.All((r) => p.AuthenticationScheme != r.LoginProvider))
+                .ToList();
+
+            ViewBag.ShowRemoveButton = userLogins.Count > 1;
+
+            var model = new ManageViewModel()
+            {
+                CurrentLogins = userLogins,
+                OtherLogins = otherLogins
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("link-account", Name = "LinkAccount")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LinkAccount(string provider)
+        {
+            await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
+
+            var redirectUrl = Url.RouteUrl("LinkAccountCallback");
+            var userId = _userManager.GetUserId(User);
+
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl, userId);
+
+            return Challenge(properties, provider);
+        }
+
+        [HttpGet]
+        [Route("link-account-callback", Name = "LinkAccountCallback")]
+        public async Task<ActionResult> LinkAccountCallback()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var userId = await _userManager.GetUserIdAsync(user);
+            var info = await _signInManager.GetExternalLoginInfoAsync(userId);
+
+            if (info == null)
+            {
+                return RedirectToAction(nameof(Index), new { Message = "Error" });
+            }
+
+            var result = await _userManager.AddLoginAsync(user, info);
+
+            var message = "Error";
+
+            if (result.Succeeded)
+            {
+                message = "LinkSuccess";
+
+                await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
+            }
+
+            return RedirectToAction(nameof(Index), new { Message = message });
+        }
+
+        [HttpPost]
+        [Route("remove-account", Name = "RemoveAccount")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveAccount(RemoveExternalService account)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var message = "Error";
+
+            if (user != null && account != null)
+            {
+                var result = await _userManager.RemoveLoginAsync(user, account.LoginProvider, account.ProviderKey);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: true);
+                    message = "RemoveSuccess";
+                }
+            }
+
+            return RedirectToAction(nameof(Index), new { Message = message });
+        }
     }
 }
---
+++ src/LondonTravel.Site/Models/ManageViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.LondonTravel.Site.Models
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Http.Authentication;
+    using Microsoft.AspNetCore.Identity;
+
+    public class ManageViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
