30481a8d91a3fa46c09b65e072a9fd313e23ac87
martin-costello <martin.costello@just-eat.com>
2017-03-08 13:37:25 +0000

    Add Terms of Service page
    Add a terms of service page.
    Refer to the privacy policy as "Privacy Policy".

---
+++ src/LondonTravel.Site/Controllers/TermsController.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.LondonTravel.Site.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// A class representing the controller for the <c>/terms-of-service/</c> resource.
+    /// </summary>
+    [Route("terms-of-service", Name = "TermsOfService")]
+    public class TermsController : Controller
+    {
+        /// <summary>
+        /// Gets the result for the <c>/terms-of-service/</c> action.
+        /// </summary>
+        /// <returns>
+        /// The result for the <c>/terms-of-service/</c> action.
+        /// </returns>
+        [HttpGet]
+        public IActionResult Index() => View();
+    }
+}
