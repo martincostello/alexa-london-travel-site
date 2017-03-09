ef23bad16840dcf9dc2379db6f9b7bdebb161c35
martincostello <martin@martincostello.com>
2017-03-09 19:19:40 +0000

    Add extension method for social CSS
    Add extension methods to handle rendering social login buttons and
    icons.

---
+++ src/LondonTravel.Site/Extensions/IHtmlHelperExtensions.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.LondonTravel.Site.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    /// <summary>
+    /// A class containing extension methods for the <see cref="IHtmlHelper"/> interface. This class cannot be inherited.
+    /// </summary>
+    public static class IHtmlHelperExtensions
+    {
+        /// <summary>
+        /// A mapping of authentication schems to icon classes. This field is read-only.
+        /// </summary>
+        private static readonly IDictionary<string, string> _iconClassMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "Amazon", "amazon" },
+            { "Facebook", "facebook" },
+            { "GitHub", "github" },
+            { "Google", "google" },
+            { "Microsoft", "windows" },
+            { "Twitter", "twitter" },
+        };
+
+        /// <summary>
+        /// Gets the CSS class to use for a social login button.
+        /// </summary>
+        /// <param name="html">The <see cref="IHtmlHelper"/> to use.</param>
+        /// <param name="authenticationScheme">The Id/name of the authentication scheme.</param>
+        /// <returns>
+        /// The CSS class to use for a button for the authentication scheme.
+        /// </returns>
+        public static string GetSocialLoginButtonCss(this IHtmlHelper html, string authenticationScheme)
+        {
+            return html.GetSocialLoginCss(authenticationScheme, "btn");
+        }
+
+        /// <summary>
+        /// Gets the CSS class to use for a social login icon.
+        /// </summary>
+        /// <param name="html">The <see cref="IHtmlHelper"/> to use.</param>
+        /// <param name="authenticationScheme">The Id/name of the authentication scheme.</param>
+        /// <returns>
+        /// The CSS class to use for an icon for the authentication scheme.
+        /// </returns>
+        public static string GetSocialLoginIconCss(this IHtmlHelper html, string authenticationScheme)
+        {
+            return html.GetSocialLoginCss(authenticationScheme, "fa");
+        }
+
+        /// <summary>
+        /// Gets the CSS class to use for a social login.
+        /// </summary>
+        /// <param name="html">The <see cref="IHtmlHelper"/> to use.</param>
+        /// <param name="authenticationScheme">The Id/name of the authentication scheme.</param>
+        /// <param name="prefix">The CSS prefix to use.</param>
+        /// <returns>
+        /// The CSS class to use for the authentication scheme.
+        /// </returns>
+        private static string GetSocialLoginCss(this IHtmlHelper html, string authenticationScheme, string prefix)
+        {
+            if (string.IsNullOrEmpty(authenticationScheme))
+            {
+                return string.Empty;
+            }
+
+            string iconClass = string.Empty;
+
+            if (_iconClassMap.TryGetValue(authenticationScheme, out string modifier))
+            {
+                iconClass = $"{prefix}-{modifier}";
+            }
+
+            return iconClass;
+        }
+    }
+}
