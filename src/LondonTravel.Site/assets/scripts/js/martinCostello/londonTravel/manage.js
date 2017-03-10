24f6965189c2378cc9c12b30192ea4c02d15f4f6
martincostello <martin@martincostello.com>
2017-03-10 21:05:39 +0000

    Remove inline script from /manage
    Remove inline JavaScript from the /manage page and bundle it.
    Fully disable the delete button before enabling, instead of just making
    it look disabled.

---
+++ src/LondonTravel.Site/assets/scripts/js/martinCostello/londonTravel/manage.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    var setupDelete = function () {
+        $("#delete-account")
+            .fadeIn("slow")
+            .removeClass("hide");
+        $("#delete-account-js").on("submit", function () {
+            $(".js-delete-control").addClass("disabled");
+            $(".js-delete-content").addClass("hide");
+            $(".js-delete-loader").removeClass("hide");
+        });
+        $(".delete-account-modal").on("show.bs.modal", function () {
+            setTimeout(function () {
+                $(".js-delete-confirm")
+                    .prop("disabled", false)
+                    .removeClass("disabled");
+            }, 3000);
+        });
+    };
+    if ("jQuery" in window) {
+        setupDelete();
+    }
+    else {
+        setTimeout(setupDelete, 1000);
+    }
+})();
