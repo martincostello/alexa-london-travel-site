945a6c1fdf7c7f0b90af29ec453614bae4375946
martincostello <martin@martincostello.com>
2017-03-11 09:39:57 +0000

    Revert strict referrer policy
    Revert the strict referrer policy as Chrome doesn't like it.

--- src/LondonTravel.Site/Middleware/CustomHttpHeadersMiddleware.cs
+++ src/LondonTravel.Site/Middleware/CustomHttpHeadersMiddleware.cs
@@ -94,7 +94,7 @@ namespace MartinCostello.LondonTravel.Site.Middleware
 
                     context.Response.Headers.Add("Content-Security-Policy", _contentSecurityPolicy);
                     context.Response.Headers.Add("Content-Security-Policy-Report-Only", _contentSecurityPolicy);
-                    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+                    context.Response.Headers.Add("Referrer-Policy", "origin-when-cross-origin");
                     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
                     context.Response.Headers.Add("X-Download-Options", "noopen");
                     context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
