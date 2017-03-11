d5781de2b862e0e887c8de5bb14895f6ab183165
martincostello <martin@martincostello.com>
2017-03-11 22:01:34 +0000

    Set moment.js locale
    Set the appropriate moment.js locale from the page language.

---
+++ src/LondonTravel.Site/assets/scripts/js/locale.js
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+(function () {
+    var locale = $("meta[name='x-request-locale']").attr("content");
+    if (moment && locale) {
+        moment.locale(locale);
+    }
+})();
