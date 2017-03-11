63c7b8dc868f14e2454671530a1b7d6753085294
martincostello <martin@martincostello.com>
2017-03-11 09:51:21 +0000

    Remove service worker registration log
    Remove the service worker registration log message as it fires on every
    page load (seemingly).

--- src/LondonTravel.Site/assets/scripts/js/serviceWorker.js
+++ src/LondonTravel.Site/assets/scripts/js/serviceWorker.js
@@ -6,7 +6,6 @@
         navigator.serviceWorker
             .register("/service-worker.js")
             .then(function () {
-                console.log("Service Worker registered.");
             })
             .catch(function (e) {
                 console.error("Failed to register Service Worker: ", e);
