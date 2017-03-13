667908adc48414490a331acda54e56a8f93d162d
martincostello <martin@martincostello.com>
2017-03-13 23:52:31 +0000

    Remove unused using statement
    Remove unused using statement from AlexaController.

--- src/LondonTravel.Site/Controllers/AlexaController.cs
+++ src/LondonTravel.Site/Controllers/AlexaController.cs
@@ -4,7 +4,6 @@
 namespace MartinCostello.LondonTravel.Site.Controllers
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Security.Cryptography;
     using System.Threading.Tasks;
