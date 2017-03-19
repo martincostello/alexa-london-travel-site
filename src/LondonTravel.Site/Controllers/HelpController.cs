744a8aec4d03eb40f9df6a39105a8d0dd7db1971
martincostello <martin@martincostello.com>
2017-03-19 17:17:16 +0000

    Add Help and Support content
    Add content to the /help page
    Resolves #43.

--- src/LondonTravel.Site/Controllers/HelpController.cs
+++ src/LondonTravel.Site/Controllers/HelpController.cs
@@ -3,14 +3,39 @@
 
 namespace MartinCostello.LondonTravel.Site.Controllers
 {
-    using System;
+    using System.Threading.Tasks;
+    using Identity;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Models;
 
     /// <summary>
     /// A class representing the controller for the <c>/help/</c> resource.
     /// </summary>
     public class HelpController : Controller
     {
+        /// <summary>
+        /// The <see cref="UserManager{TUser}"/> to use. This field is read-only.
+        /// </summary>
+        private readonly UserManager<LondonTravelUser> _userManager;
+
+        /// <summary>
+        /// The <see cref="ILogger"/> to use. This field is read-only.
+        /// </summary>
+        private readonly ILogger<HelpController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HelpController"/> class.
+        /// </summary>
+        /// <param name="userManager">The <see cref="UserManager{TUser}"/> to use.</param>
+        /// <param name="logger">The <see cref="ILogger"/> to use.</param>
+        public HelpController(UserManager<LondonTravelUser> userManager, ILogger<HelpController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
         /// <summary>
         /// Gets the result for the <c>/help/</c> action.
         /// </summary>
@@ -19,7 +44,23 @@ namespace MartinCostello.LondonTravel.Site.Controllers
         /// </returns>
         [HttpGet]
         [Route("help", Name = SiteRoutes.Help)]
-        public IActionResult Index() => View();
+        public async Task<IActionResult> Index()
+        {
+            var model = new HelpViewModel()
+            {
+                IsSignedIn = User.Identity.IsAuthenticated,
+            };
+
+            if (model.IsSignedIn)
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                model.HasFavorites = user?.FavoriteLines?.Count > 0;
+                model.IsLinkedToAlexa = !string.IsNullOrEmpty(user?.AlexaToken);
+            }
+
+            return View(model);
+        }
 
         /// <summary>
         /// Gets the result for the <c>/support/</c> action.
