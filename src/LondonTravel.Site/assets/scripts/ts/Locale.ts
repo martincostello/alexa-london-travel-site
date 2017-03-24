93dafd786e97914d597976abc92b41fefb3570b6
martin-costello <martin.costello@just-eat.com>
2017-03-24 15:15:05 +0000

    Convert locale.js to TypeScript
    Convert locale.js to TypeScript.

--- src/LondonTravel.Site/assets/scripts/ts/Locale.ts
+++ src/LondonTravel.Site/assets/scripts/ts/Locale.ts
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-(function () {
-    var locale = $("meta[name='x-request-locale']").attr("content");
-    if (moment && locale) {
+declare var moment: any;
+
+(() => {
+    let locale: string = $("meta[name='x-request-locale']").attr("content");
+    if ("moment" in window && locale) {
         moment.locale(locale);
     }
 })();
