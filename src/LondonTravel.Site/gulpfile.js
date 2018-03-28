63fa6491214f99b685df7ab68dd06e9ce542511d
martincostello <martin@martincostello.com>
2018-03-28 12:34:53 +0100

    Add gulp binding for project open
    Run gulp publish when the site project is opened.

--- src/LondonTravel.Site/gulpfile.js
+++ src/LondonTravel.Site/gulpfile.js
@@ -1,5 +1,4 @@
-﻿/// <binding Clean='clean' ProjectOpened='watch' />
-
+/// <binding Clean='clean' ProjectOpened='publish, watch' />
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
