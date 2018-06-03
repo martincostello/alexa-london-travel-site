dc2c0b53feb27b71cd84aa4b75537cc9470a4b69
martincostello <martin@martincostello.com>
2018-06-03 13:52:19 +0100

    Add integration tests for external authentication
    Add integration tests for external authentication with all of the integrated providers.
    Log intercepted HTTP requests.
    Configure WebDriver timeouts.

--- tests/LondonTravel.Site.Tests/Integration/AuthenticationTests.cs
+++ tests/LondonTravel.Site.Tests/Integration/AuthenticationTests.cs
@@ -4,26 +4,98 @@
 namespace MartinCostello.LondonTravel.Site.Integration
 {
     using System;
-    using System.Threading.Tasks;
+    using Builders;
+    using OpenQA.Selenium;
+    using Shouldly;
+    using Xunit.Abstractions;
 
     /// <summary>
     /// A class containing tests for authentication providers in the website.
     /// </summary>
-    public class AuthenticationTests : BrowserTest
+    public sealed class AuthenticationTests : BrowserTest
     {
-        public AuthenticationTests(HttpServerFixture fixture)
-            : base(fixture)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthenticationTests"/> class.
+        /// </summary>
+        /// <param name="fixture">The fixture to use.</param>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public AuthenticationTests(HttpServerFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper)
         {
         }
 
         [NotCIFact]
-        public async Task Can_Load_Homepage()
+        public void Can_Sign_In_With_Amazon()
         {
+            // Arrange
+            ConfigureExternalProvider((p) => p.ForAmazon());
+
+            // Act and Assert
+            SignInWithExternalProvider("amazon");
+        }
+
+        [NotCIFact]
+        public void Can_Sign_In_With_Facebook()
+        {
+            // Arrange
+            ConfigureExternalProvider((p) => p.ForFacebook());
+
+            // Act and Assert
+            SignInWithExternalProvider("facebook");
+        }
+
+        [NotCIFact]
+        public void Can_Sign_In_With_Google()
+        {
+            // Arrange
+            ConfigureExternalProvider((p) => p.ForGoogle());
+
+            // Act and Assert
+            SignInWithExternalProvider("google");
+        }
+
+        [NotCIFact]
+        public void Can_Sign_In_With_Microsoft()
+        {
+            // Arrange
+            ConfigureExternalProvider((p) => p.ForMicrosoft());
+
+            // Act and Assert
+            SignInWithExternalProvider("microsoft");
+        }
+
+        [NotCIFact]
+        public void Can_Sign_In_With_Twitter()
+        {
+            // Arrange
+            ConfigureExternalProvider(
+                (p) =>
+                {
+                    p.Tokens().WithAccessToken("twitter-oath-token");
+                    p.ForTwitter();
+                });
+
+            // Act and Assert
+            SignInWithExternalProvider("twitter", "@JohnSmith");
+        }
+
+        private void ConfigureExternalProvider(Action<AuthenticationInterceptionBuilder> configure)
+        {
+            configure(new AuthenticationInterceptionBuilder(Fixture.Interceptor));
+        }
+
+        private void SignInWithExternalProvider(string providerName, string expectedName = "John")
+        {
+            // Act
             using (var driver = CreateWebDriver())
             {
                 driver.Navigate().GoToUrl(Fixture.ServerAddress);
 
-                await Task.Delay(TimeSpan.FromSeconds(5));
+                driver.FindElement(By.CssSelector("[data-id='sign-in']")).Click();
+                driver.FindElement(By.CssSelector($"[data-id='sign-in-{providerName}']")).Click();
+
+                // Assert
+                driver.FindElement(By.CssSelector("[data-id='user-name']")).Text.ShouldBe(expectedName);
             }
         }
     }
--- tests/LondonTravel.Site.Tests/Integration/BrowserTest.cs
+++ tests/LondonTravel.Site.Tests/Integration/BrowserTest.cs
@@ -3,29 +3,70 @@
 
 namespace MartinCostello.LondonTravel.Site.Integration
 {
+    using System;
+    using System.Threading.Tasks;
+    using JustEat.HttpClientInterception;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using Xunit;
+    using Xunit.Abstractions;
 
     /// <summary>
     /// The base class for browser tests.
     /// </summary>
     [Collection(HttpServerCollection.Name)]
-    public abstract class BrowserTest
+    public abstract class BrowserTest : IDisposable
     {
+        private bool _disposed;
+        private IDisposable _scope;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="BrowserTest"/> class.
         /// </summary>
         /// <param name="fixture">The fixture to use.</param>
-        protected BrowserTest(HttpServerFixture fixture)
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        protected BrowserTest(HttpServerFixture fixture, ITestOutputHelper outputHelper)
         {
             Fixture = fixture;
+            Output = outputHelper;
+
+            Fixture.Interceptor.OnSend = (request) =>
+            {
+                Output.WriteLine($"[{DateTimeOffset.UtcNow:u}] {request.ToString()}");
+                return Task.CompletedTask;
+            };
+
+            _scope = Fixture.Interceptor.BeginScope();
+
+            new Builders.TflInterceptionBuilder().ForLines().RegisterWith(Fixture.Interceptor);
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="BrowserTest"/> class.
+        /// </summary>
+        ~BrowserTest()
+        {
+            Dispose(false);
         }
 
         /// <summary>
         /// Gets the <see cref="HttpServerFixture"/> to use.
         /// </summary>
         protected HttpServerFixture Fixture { get; }
+
+        /// <summary>
+        /// Gets the <see cref="ITestOutputHelper"/> to use.
+        /// </summary>
+        protected ITestOutputHelper Output { get; }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
         /// <summary>
         /// Creates a new instance of <see cref="IWebDriver"/>.
@@ -47,7 +88,45 @@ namespace MartinCostello.LondonTravel.Site.Integration
                 options.AddArgument("--headless");
             }
 
-            return new ChromeDriver(chromeDriverDirectory, options);
+            var driver = new ChromeDriver(chromeDriverDirectory, options);
+
+            try
+            {
+                var timeout = TimeSpan.FromSeconds(10);
+                var timeouts = driver.Manage().Timeouts();
+
+                timeouts.AsynchronousJavaScript = timeout;
+                timeouts.ImplicitWait = timeout;
+                timeouts.PageLoad = timeout;
+            }
+            catch (Exception)
+            {
+                driver.Dispose();
+                throw;
+            }
+
+            return driver;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <see langword="true" /> to release both managed and unmanaged resources;
+        /// <see langword="false" /> to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _scope?.Dispose();
+                    _scope = null;
+                }
+
+                _disposed = true;
+            }
         }
     }
 }
