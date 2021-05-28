2801402947993c065b57ca0f35d50a7d035ac4c6
martincostello <martin@martincostello.com>
2021-05-28 20:30:59 +0100

    Fix typo
    Fix typo in error message.

--- tests/LondonTravel.Site.Tests/BrowserFixture.cs
+++ tests/LondonTravel.Site.Tests/BrowserFixture.cs
@@ -110,7 +110,7 @@ namespace MartinCostello.LondonTravel.Site
             }
             catch (Exception ex)
             {
-                OutputHelper.WriteLine("Failedto capture screenshot: " + ex);
+                OutputHelper.WriteLine("Failed to capture screenshot: " + ex);
             }
         }
 
