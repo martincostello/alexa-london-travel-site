4bf4f86935d80a8eab4b63f5d7c80ccacfa8919f
martincostello <martin@martincostello.com>
2021-10-17 16:07:40 +0100

    Run dotnet format
    Run dotnet format on the solution.

--- tests/LondonTravel.Site.Tests/Extensions/ClaimsPrincipalExtensionsTests.cs
+++ tests/LondonTravel.Site.Tests/Extensions/ClaimsPrincipalExtensionsTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Security.Claims;
