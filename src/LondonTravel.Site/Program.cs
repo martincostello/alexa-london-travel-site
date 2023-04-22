7f70400f5313389d9a1f2615b3fe0798cc8707b7
Martin Costello <martin@martincostello.com>
2023-04-22 09:19:09 +0100

    Remove redundant suppression
    Remove now-redundant suppression for false-positive code analysis warning.

--- src/LondonTravel.Site/Program.cs
+++ src/LondonTravel.Site/Program.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-#pragma warning disable CA1852
-
 using System.IO.Compression;
 using System.Text.Json.Serialization.Metadata;
 using MartinCostello.LondonTravel.Site;
