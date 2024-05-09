87735e4a8e579f5f2f6512fd5b76c2842e0bd3ac
martincostello <martin@martincostello.com>
2024-05-09 10:32:09 +0100

    Switch to Azure Monitor exporter
    Switch to Azure.Monitor.OpenTelemetry.Exporter from Azure.Monitor.OpenTelemetry.AspNetCore as the latter is not AoT friendly.

--- src/LondonTravel.Site/Extensions/ILoggingBuilderExtensions.cs
+++ src/LondonTravel.Site/Extensions/ILoggingBuilderExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Azure.Monitor.OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 
 namespace MartinCostello.LondonTravel.Site.Extensions;
@@ -19,14 +20,19 @@ public static class ILoggingBuilderExtensions
     /// </returns>
     public static ILoggingBuilder AddTelemetry(this ILoggingBuilder builder)
     {
-        return builder.AddOpenTelemetry((p) =>
+        return builder.AddOpenTelemetry((options) =>
         {
-            p.IncludeFormattedMessage = true;
-            p.IncludeScopes = true;
+            options.IncludeFormattedMessage = true;
+            options.IncludeScopes = true;
+
+            if (TelemetryExtensions.IsAzureMonitorConfigured())
+            {
+                options.AddAzureMonitorLogExporter();
+            }
 
             if (TelemetryExtensions.IsOtlpCollectorConfigured())
             {
-                p.AddOtlpExporter();
+                options.AddOtlpExporter();
             }
         });
     }
