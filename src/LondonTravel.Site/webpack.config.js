76d7ae4825c0fe43f1359624f8b5d56d68779292
martincostello <martin@martincostello.com>
2024-05-19 08:43:09 +0100

    Update obsolete linter rule
    - Remove obsolete ESLint rule.
    - Fix lint violations in webpack configuration.
    - Configure webpack for potential future use of react.

--- src/LondonTravel.Site/webpack.config.js
+++ src/LondonTravel.Site/webpack.config.js
@@ -1,52 +1,52 @@
 const path = require('path');
 const webpack = require('webpack');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const removeEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    css: path.resolve(__dirname, './assets/styles/main.css'),
-    js: path.resolve(__dirname, './assets/scripts/main.ts'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: "css-loader", options: { sourceMap: true } },
+    devtool: 'source-map',
+    entry: {
+        css: path.resolve(__dirname, './assets/styles/main.css'),
+        js: path.resolve(__dirname, './assets/scripts/main.ts'),
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
-      },
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            '...',
+            new cssMinimizerPlugin(),
+        ],
+    },
+    output: {
+        clean: true,
+        filename: '[name]/main.js',
+        path: path.resolve(__dirname, 'wwwroot', 'assets'),
+    },
+    plugins: [
+        new miniCssExtractPlugin({
+            filename: '[name]/main.css'
+        }),
+        new removeEmptyScriptsPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en-gb/),
     ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      `...`,
-      new CssMinimizerPlugin(),
-    ],
-  },
-  output: {
-    clean: true,
-    filename: '[name]/main.js',
-    path: path.resolve(__dirname, 'wwwroot', 'assets'),
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name]/main.css'
-    }),
-    new RemoveEmptyScriptsPlugin(),
-    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en-gb/),
-  ],
-  resolve: {
-    extensions: ['.css', '.ts', '.js'],
-  },
+    resolve: {
+        extensions: ['.css', '.tsx', '.ts', '.js'],
+    },
 };
