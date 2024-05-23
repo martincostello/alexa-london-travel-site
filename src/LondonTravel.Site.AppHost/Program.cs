1ca52edf2f30eb9bf10b062a93797f59d8e36422
Martin Costello <martin@martincostello.com>
2024-05-23 09:13:46 +0100

    Revert to .NET Aspire 8.0.1 (#2462)
    There won't be any further previews of .NET Aspire for .NET 9 previews.

--- src/LondonTravel.Site.AppHost/Program.cs
+++ src/LondonTravel.Site.AppHost/Program.cs
@@ -3,12 +3,11 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//// HACK Disabled until there are .NET 9 preview.4 packages available
-////var cosmos = builder.AddAzureCosmosDB("Cosmos")
-////                   .RunAsEmulator();
+var cosmos = builder.AddAzureCosmosDB("Cosmos")
+                    .RunAsEmulator();
 
-builder.AddProject<Projects.LondonTravel_Site>("LondonTravelSite");
-//// .WithReference(cosmos);
+builder.AddProject<Projects.LondonTravel_Site>("LondonTravelSite")
+       .WithReference(cosmos);
 
 var app = builder.Build();
 
