125e9bc6d6f15e002540f76d10b3ae0e6007bb22
martincostello <martin@martincostello.com>
2024-07-27 16:26:13 +0100

    Update to ASP.NET Core 9
    - Update to ASP.NET Core 9 using preview 6.
    - Use new `Convert.ToHexStringLower()` API.
    - Replace Swashbuckle.AspNetCore with Microsoft.AspNetCore.OpenApi.

--- src/LondonTravel.Site/OpenApi/RemoveStyleCopPrefixes.cs
+++ src/LondonTravel.Site/OpenApi/RemoveStyleCopPrefixes.cs
@@ -1,34 +1,31 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MartinCostello.LondonTravel.Site.OpenApi;
 
-internal sealed class RemoveStyleCopPrefixes : ISchemaFilter
+internal sealed class RemoveStyleCopPrefixes : IOpenApiSchemaTransformer
 {
     private const string Prefix = "Gets or sets ";
 
-    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    public Task TransformAsync(
+        OpenApiSchema schema,
+        OpenApiSchemaTransformerContext context,
+        CancellationToken cancellationToken)
     {
-        if (schema.Description is not null)
+        if (schema.Description is { } description)
         {
-            schema.Description = schema.Description.Replace("`", string.Empty, StringComparison.Ordinal);
+            schema.Description = description.Replace("`", string.Empty, StringComparison.Ordinal);
         }
 
-        foreach (var property in schema.Properties.Values)
-        {
-            TryUpdateDescription(property);
-        }
-    }
-
-    private static void TryUpdateDescription(OpenApiSchema property)
-    {
-        if (property.Description is not null)
+        foreach (var property in schema.Properties.Values.Where((p) => p.Description is not null))
         {
             property.Description = property.Description.Replace(Prefix, string.Empty, StringComparison.Ordinal);
             property.Description = char.ToUpperInvariant(property.Description[0]) + property.Description[1..];
         }
+
+        return Task.CompletedTask;
     }
 }
