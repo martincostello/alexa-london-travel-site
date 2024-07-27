966e0429625b59b29e7f9aeb77454ad86ddc027f
martincostello <martin@martincostello.com>
2024-07-27 16:26:13 +0100

    Update to ASP.NET Core 9
    - Update to ASP.NET Core 9 using preview 6.
    - Use new `Convert.ToHexStringLower()` API.
    - Replace Swashbuckle.AspNetCore with Microsoft.AspNetCore.OpenApi.

--- src/LondonTravel.Site/OpenApi/AddServers.cs
+++ src/LondonTravel.Site/OpenApi/AddServers.cs
@@ -1,19 +1,23 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MartinCostello.LondonTravel.Site.OpenApi;
 
 internal sealed class AddServers(
     IHttpContextAccessor accessor,
-    IOptions<ForwardedHeadersOptions> options) : IDocumentFilter
+    IOptions<ForwardedHeadersOptions> options) : IOpenApiDocumentTransformer
 {
-    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    public Task TransformAsync(
+        OpenApiDocument document,
+        OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken)
     {
-        swaggerDoc.Servers = [new() { Url = GetServerUrl(accessor, options.Value) }];
+        document.Servers = [new() { Url = GetServerUrl(accessor, options.Value) }];
+        return Task.CompletedTask;
     }
 
     private static string GetServerUrl(IHttpContextAccessor accessor, ForwardedHeadersOptions options)
