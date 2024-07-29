f17312144f04d9e26bcb931c86f9974799eca951
martincostello <martin@martincostello.com>
2024-07-29 12:12:34 +0100

    Use MartinCostello.OpenApi.Extensions
    Use MartinCostello.OpenApi.Extensions NuGet package to extend OpenAPI document.

--- src/LondonTravel.Site/OpenApi/IExampleProvider`1.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Martin Costello, 2017. All rights reserved.
-// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
-
-namespace MartinCostello.LondonTravel.Site.OpenApi;
-
-internal interface IExampleProvider<T>
-{
-    static abstract T GenerateExample();
-}
