638ffe08ef15b74b0609084a23747f76d45f9310
martincostello <martin@martincostello.com>
2024-08-13 16:15:16 +0100

    Update to ASP.NET Core 9 preview 7
    Update to preview 7 of ASP.NET Core 9.

--- tests/LondonTravel.Site.Tests/Integration/HttpServerFixture.cs
+++ tests/LondonTravel.Site.Tests/Integration/HttpServerFixture.cs
@@ -111,7 +111,7 @@ public sealed class HttpServerFixture : TestServerFixture
 
         builder.ConfigureKestrel(
             (p) => p.ConfigureHttpsDefaults(
-                (r) => r.ServerCertificate = new X509Certificate2("localhost-dev.pfx", "Pa55w0rd!")));
+                (r) => r.ServerCertificate = X509CertificateLoader.LoadPkcs12FromFile("localhost-dev.pfx", "Pa55w0rd!")));
 
         // Configure the server address for the server to
         // listen on for HTTPS requests on a dynamic port.
