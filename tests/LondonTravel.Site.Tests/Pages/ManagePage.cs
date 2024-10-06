938a5aa1be280ff2e421a5ce49b0f6ce4a103183
martincostello <martin@martincostello.com>
2024-10-06 11:31:40 +0100

    Remove redundant usings
    Remove redundant `using` statements.

--- tests/LondonTravel.Site.Tests/Pages/ManagePage.cs
+++ tests/LondonTravel.Site.Tests/Pages/ManagePage.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using Microsoft.Playwright;
-
 namespace MartinCostello.LondonTravel.Site.Pages;
 
 public sealed class ManagePage(ApplicationNavigator navigator) : PageBase(navigator)
