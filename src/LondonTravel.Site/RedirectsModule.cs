ea8ceba289773d591c6129e07325122cf773d41a
martincostello <martin@martincostello.com>
2025-03-01 13:27:21 +0000

    Configurable crawlers
    Make the URLs to deflect crawlers configurable.

--- src/LondonTravel.Site/RedirectsModule.cs
+++ src/LondonTravel.Site/RedirectsModule.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Security.Cryptography;
+using MartinCostello.LondonTravel.Site.Options;
+using Microsoft.Extensions.Options;
 
 namespace MartinCostello.LondonTravel.Site;
 
@@ -55,53 +57,11 @@ public static class RedirectsModule
             Redirect(originalPath, newPath);
         }
 
-        string[] crawlerPaths =
-        [
-            ".env",
-            ".git/{*catchall}",
-            "admin.php",
-            "admin-console/{*catchall}",
-            "admin/{*catchall}",
-            "administration/{*catchall}",
-            "administrator/{*catchall}",
-            "appsettings.json",
-            "ajaxproxy/{*catchall}",
-            "bin/{*catchall}",
-            "bitrix/admin/{*catchall}",
-            "blog/{*catchall}",
-            "cms/{*catchall}",
-            "index.php",
-            "invoker/JMXInvokerServlet",
-            "jmx-console/HtmlAdaptor",
-            "license.php",
-            "magmi/web/magmi.php",
-            "manager/index.php",
-            "modules/{*catchall}",
-            "obj/{*catchall}",
-            "package.json",
-            "package-lock.json",
-            "parameters.xml",
-            "readme.htm",
-            "readme.html",
-            "site/{*catchall}",
-            "sites/{*catchall}",
-            "tiny_mce/{*catchall}",
-            "uploadify/{*catchall}",
-            "web.config",
-            "web-console/Invoker",
-            "wordpress/{*catchall}",
-            "wp/{*catchall}",
-            "wp-admin/{*catchall}",
-            "wp-content/{*catchall}",
-            "wp-includes/{*catchall}",
-            "wp-links-opml.php",
-            "wp-login.php",
-            "xmlrpc.php",
-        ];
+        var options = app.ServiceProvider.GetRequiredService<IOptions<SiteOptions>>();
 
         string[] httpMethods = ["GET", "HEAD", "POST"];
 
-        foreach (string path in crawlerPaths)
+        foreach (string path in options.Value.CrawlerPaths)
         {
             RedirectCrawler(path);
         }
