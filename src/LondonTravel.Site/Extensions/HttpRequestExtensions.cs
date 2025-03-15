76a172dc337958f86dbedb7fb0154fb0b207a1f3
martincostello <martin@martincostello.com>
2025-03-15 16:14:29 +0000

    Avoid Uri allocations
    - Construct CDN URLs with strings to avoid `Uri` allocations.
    - Remove redundant DNS preload domains.

--- src/LondonTravel.Site/Extensions/HttpRequestExtensions.cs
+++ src/LondonTravel.Site/Extensions/HttpRequestExtensions.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using MartinCostello.LondonTravel.Site.Options;
+
 namespace MartinCostello.LondonTravel.Site.Extensions;
 
 /// <summary>
@@ -45,5 +47,60 @@ public static class HttpRequestExtensions
         }
 
         return canonicalUri;
+    }
+
+    /// <summary>
+    /// Converts a virtual (relative) path to an CDN absolute URI, if configured.
+    /// </summary>
+    /// <param name="value">The <see cref="HttpRequest"/>.</param>
+    /// <param name="contentPath">The virtual path of the content.</param>
+    /// <param name="options">The current site configuration.</param>
+    /// <returns>The CDN absolute URI, if configured; otherwise the application absolute URI.</returns>
+    public static string CdnContent(this HttpRequest value, string contentPath, SiteOptions options)
+    {
+        var cdn = options?.ExternalLinks?.Cdn;
+
+        // Prefer empty images to a NullReferenceException
+        if (cdn == null)
+        {
+            return string.Empty;
+        }
+
+        return $"{cdn}london-travel_{value.Content(contentPath)?.TrimStart('/')}";
+    }
+
+    /// <summary>
+    /// Converts a virtual (relative) path to a relative URI.
+    /// </summary>
+    /// <param name="request">The <see cref="HttpRequest"/>.</param>
+    /// <param name="contentPath">The virtual path of the content.</param>
+    /// <param name="appendVersion">Whether to append a version to the URL.</param>
+    /// <returns>The relatve URI to the content.</returns>
+    public static string? Content(this HttpRequest request, string? contentPath, bool appendVersion = true)
+    {
+        string? result = string.Empty;
+
+        if (!string.IsNullOrEmpty(contentPath))
+        {
+            if (contentPath[0] == '~')
+            {
+                var segment = new PathString(contentPath[1..]);
+                var applicationPath = request.PathBase;
+
+                var path = applicationPath.Add(segment);
+                result = path.Value;
+            }
+            else
+            {
+                result = contentPath;
+            }
+        }
+
+        if (appendVersion)
+        {
+            result += $"?v={GitMetadata.Commit}";
+        }
+
+        return result;
     }
 }
--- src/LondonTravel.Site/Extensions/IUrlHelperExtensions.cs
+++ src/LondonTravel.Site/Extensions/IUrlHelperExtensions.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using MartinCostello.LondonTravel.Site.Options;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MartinCostello.LondonTravel.Site.Extensions;
@@ -21,38 +20,6 @@ public static class IUrlHelperExtensions
     {
         var request = value.ActionContext.HttpContext.Request;
         return value.ToAbsolute(request.Host.Value ?? string.Empty, contentPath);
-    }
-
-    /// <summary>
-    /// Converts a virtual (relative) path to an CDN absolute URI, if configured.
-    /// </summary>
-    /// <param name="value">The <see cref="IUrlHelper"/>.</param>
-    /// <param name="contentPath">The virtual path of the content.</param>
-    /// <param name="options">The current site configuration.</param>
-    /// <param name="appendVersion">Whether to append a version query string parameter to the URL.</param>
-    /// <returns>The CDN absolute URI, if configured; otherwise the application absolute URI..</returns>
-    public static string CdnContent(this IUrlHelper value, string contentPath, SiteOptions? options, bool appendVersion = true)
-    {
-        var cdn = options?.ExternalLinks?.Cdn;
-
-        // Prefer empty images to a NullReferenceException
-        if (cdn == null)
-        {
-            return string.Empty;
-        }
-
-        // Azure Blob storage is case-sensitive, so force all URLs to lowercase
-#pragma warning disable CA1308
-        string url = value.ToAbsolute(cdn.Host, $"london-travel_{contentPath.ToLowerInvariant().TrimStart('/')}");
-#pragma warning restore CA1308
-
-        // asp-append-version="true" does not work for non-local resources
-        if (appendVersion)
-        {
-            url += $"?v={GitMetadata.Commit}";
-        }
-
-        return url;
     }
 
     /// <summary>
