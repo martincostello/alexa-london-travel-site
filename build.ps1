ec062fa701fd2a77de632bc504b7c79a33b25980
martincostello <martin@martincostello.com>
2025-04-18 10:42:15 +0100

    Add test analytics
    Use Codecov's test analytics feature.

--- build.ps1
+++ build.ps1
@@ -72,8 +72,8 @@ function DotNetTest {
     $additionalArgs = @()
 
     if (![string]::IsNullOrEmpty(${env:GITHUB_SHA})) {
-        $additionalArgs += "--logger"
-        $additionalArgs += "GitHubActions;report-warnings=false"
+        $additionalArgs += "--logger:GitHubActions;report-warnings=false"
+        $additionalArgs += "--logger:junit;LogFilePath=junit.xml"
     }
 
     & $dotnet test --configuration "Release" $additionalArgs
