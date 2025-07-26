542e7339d367d90ef7302b908fec55877a3ad58d
Martin Costello <martin@martincostello.com>
2025-07-26 12:46:29 +0100

    Fail benchmarks if any errors
    Fail the benchmarks if any errors are encountered during execution.

--- benchmark.ps1
+++ benchmark.ps1
@@ -80,3 +80,8 @@ if (-Not [string]::IsNullOrEmpty(${env:GITHUB_SHA})) {
 }
 
 & $dotnet run --project $benchmarks --configuration "Release" -- $additionalArgs
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Benchmarks failed with exit code $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
